2f52798ba954f46f9b8ab3bee0b80a9475b88c88
aochwang <aochwang@gmail.com>
2019-05-23 23:56:31 +0800

    headers 处理

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import { buildURL } from "./helpers/url";
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/header';
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
@@ -10,7 +11,9 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config);
-  config.data = transformRequestData(config)
+  config.headers = transformHeaders(config);
+  // 最后处理data! data处理过后就变成 json字符串了，headers中判断会失败
+  config.data = transformRequestData(config);
 }
 
 function transformURL(config: AxiosRequestConfig): string {
@@ -21,6 +24,11 @@ function transformURL(config: AxiosRequestConfig): string {
 function transformRequestData(config: AxiosRequestConfig): any {
   const { data } = config;
   return transformRequest(data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config;
+  return processHeaders(headers, data);
 }
 
 export default axios
